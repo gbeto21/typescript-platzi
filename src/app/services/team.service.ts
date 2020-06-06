d6c1b2b44943798d549a00b4e5528d46d6fa1525
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-06 13:10:30 -0500

    Se crearon los servicios de teams y player. Se termino la clase: 'Creacion e implementacion de endpoints en Firebase.'

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Team } from '../interfaces/team';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export const TeamsTableHeaders = ['name', 'country', 'players']
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+
+  private teamsDb: AngularFireList<Team>
+
+  constructor(private db: AngularFireDatabase) {
+    this.teamsDb = this.db.list('/teams', ref => ref.orderByChild('name'));
+  }
+
+  getTeams(): Observable<Team[]>{
+    return this.teamsDb.snapshotChanges().pipe(
+      map(changes =>{
+        return changes.map(c=>({ $key: c.payload.key, ...c.payload.val() }))
+      })
+    )
+  }
+
+  addPlayer(team: Team){
+    return this.teamsDb.push(team);
+  }
+
+  deleteTeam(id: string){
+    this.db.list('/teams').remove(id);
+  }
+
+  editPlayer(newTeamData){
+    const $key = newTeamData.$key;
+    delete(newTeamData.$key);
+    this.db.list('/teams').update($key, newTeamData)
+  }
+
+}
