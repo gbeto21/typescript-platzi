bc12e287f21d5e742da782f633ed5aead8e2142d
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-06 17:16:14 -0500

    Se terminó la clase ¿Qué es FirebaseÂ?

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6cnowoqAnYyg4-pt4Urdd_JGfXG7fPOs",
+    authDomain: "typescript-platzi-17ae5.firebaseapp.com",
+    databaseURL: "https://typescript-platzi-17ae5.firebaseio.com",
+    projectId: "typescript-platzi-17ae5",
+    storageBucket: "typescript-platzi-17ae5.appspot.com",
+    messagingSenderId: "395525905364",
+    appId: "1:395525905364:web:d4a3b1cc7f9ec8b89e5441"
+  }
 };
 
 /*
